f1c6fb1df03ecbeedf3cf8e23704f24ca880f7ae
roumanite <madyagra@gmail.com>
2019-09-07 12:24:56 +0800

    Convert 12 hour AM/PM format to military (24-hour) time

---
+++ TimeFormatting/unoptimized_solution.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the timeConversion function below.
+ */
+
+/*
+ * Please either make the string static or allocate on the heap. For example,
+ * static char str[] = "hello world";
+ * return str;
+ *
+ * OR
+ *
+ * char* str = "hello world";
+ * return str;
+ *
+ */
+char* timeConversion(char* s) {
+    char * time = (char*)calloc(strlen(s) - 1, sizeof(*time));
+    strncpy(time, s, sizeof(time));
+
+    char * pointer;
+    long h = strtol(s, &pointer, 10);
+
+    if(s[8] == 'P' && h != 12){
+        h = 12 + h; 
+        char tmp[3];
+        sprintf(tmp, "%ld", h);
+        time[0] = tmp[0];
+        time[1] = tmp[1];
+    }
+    if(s[8] == 'A' && h == 12){
+        time[0] = '0';
+        time[1] = '0'; 
+    }
+
+    return time;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
